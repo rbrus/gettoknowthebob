a815f3312e5b0a3bb518ddc19c2d490354fb5e28
rbrus <radoslaw.brus@gmail.com>
2016-04-09 08:31:16 +0100

    Commiting AtManager (AT commands software tool)

---
+++ csharp/AtManager/AtManager/ResponseItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtManager
+{
+    public class ResponseItem
+    {
+        public ResponseValue ResponseValue { get; set; }
+        public string ResponseData { get; set; }
+    }
+}
