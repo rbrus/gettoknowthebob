980131c86113df6640b315415701d4201fc9d04e
rbrus <radoslaw.brus@gmail.com>
2016-04-24 20:52:00 +0100

    24.04.2016 Thermometer Nodemcu + DS18B20

--- arduino/nodemcu/DS18B20/DS18B20.ino
+++ arduino/nodemcu/DS18B20/DS18B20.ino
@@ -2,13 +2,13 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
-#define myPeriodic 15 //in sec | Thingspeak pub is 15sec
+#define myPeriodic 15   // Seconds
 #define ONE_WIRE_BUS 2  // DS18B20 on arduino pin2 corresponds to D4 on physical board
 
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature DS18B20(&oneWire);
 float prevTemp = 0;
-const char* MY_SSID = "Fred's Network";
+const char* MY_SSID = "Fred's Network"; 
 const char* MY_PWD = "Leos12072014#";
 int sent = 0;
 void setup() {
@@ -17,12 +17,13 @@ void setup() {
 
 void loop() {
   float temp;
-  DS18B20.requestTemperatures();
+  DS18B20.requestTemperatures(); 
   temp = DS18B20.getTempCByIndex(0);
   Serial.print(String(sent)+" Temperature: ");
   Serial.println(temp);
-
+  sent++;
   int count = myPeriodic;
   while(count--)
   delay(1000);
 }
+
