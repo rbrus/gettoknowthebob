ac54e70246b58eabce25dfba15c01c3c434cd4ea
rbrus <radoslaw.brus@gmail.com>
2016-05-22 17:46:46 +0100

    Things Speak Project with DS18B20

---
+++ arduino/nodemcu/ThingSpeakDS18B20/ThingSpeakDS18B20.ino
@@ -0,0 +1,81 @@
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+#define myPeriodic 15   // Seconds
+#define ONE_WIRE_BUS 2  // DS18B20 on arduino pin2 corresponds to D4 on physical board
+
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature DS18B20(&oneWire);
+WiFiClient client;
+
+// replace with your channel’s thingspeak API key,
+String apiKey = "B9HSYHJWRKYUILAA";
+const char* ssid = "ssid";
+const char* password = "password";
+const char* server = "api.thingspeak.com";
+
+//int sent = 0;
+
+void setup() {
+  Serial.begin(115200);
+  delay(10);
+
+  WiFi.begin(ssid, password);
+  
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+  delay(500);
+  Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+}
+
+void loop() {
+  
+  float temp;
+  DS18B20.requestTemperatures(); 
+  temp = DS18B20.getTempCByIndex(0);
+  
+  //Serial.print(String(sent)+" Temperature: ");
+  //Serial.println(temp);
+  //sent++;
+  //int count = myPeriodic;
+  //while(count--)
+  //delay(1000);
+
+  if (client.connect(server,80)) { // "184.106.153.149" or api.thingspeak.com
+  String postStr = apiKey;
+  postStr +="&field1=";
+  postStr += String(temp);
+  postStr += "\r\n\r\n";
+  
+  client.print("POST /update HTTP/1.1\n");
+  client.print("Host: api.thingspeak.com\n");
+  client.print("Connection: close\n");
+  client.print("X-THINGSPEAKAPIKEY: "+apiKey+"\n");
+  client.print("Content-Type: application/x-www-form-urlencoded\n");
+  client.print("Content-Length: ");
+  client.print(postStr.length());
+  client.print("\n\n");
+  client.print(postStr);
+  
+  Serial.print("Temperature: ");
+  Serial.print(temp);
+  Serial.println("% send to Thingspeak");
+  }
+  client.stop();
+  
+  Serial.println("Waiting…");
+  // thingspeak needs minimum 15 sec delay between updates
+  delay(20000);
+}
+
